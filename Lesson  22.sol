3e311e80d390c7186a85b900c5d8e77833f36791
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-04-03 15:00:22 +0100

    Update Lesson  22.sol
    updates

--- Lesson  22.sol
+++ Lesson  22.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+contract Lesson{
+ string public immutable i_text = "kehinde";
+
+mapping(address => mapping(address=> uint256) public balances);
+
+
+
+}
