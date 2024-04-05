436af5be83eafcc6d4b3772c63ff15c538a56ef7
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-04-05 13:26:53 +0100

    Update Lesson  22.sol
    COMPLETED LESSON 22

--- Lesson  22.sol
+++ Lesson  22.sol
@@ -2,10 +2,33 @@
 pragma solidity ^0.8.19;
 contract Lesson{
  string public immutable i_text = "kehinde";
-
 mapping(address => mapping(address=> uint256) public balances);
 Balances[] public balances;
 
+function getBalance() external{
+balances = _balance;
+return address(this).balance;
+}
+
+    struct Balance {
+        address from;
+        address to;
+        uint256 amount;
+    }
+
+    function getBalance(address from, address to) external view returns (uint256) {
+        return balances[from][to];
+    }
+
+    function updateBalance(address from, address to, uint256 amount) external {
+        balances[from][to] = amount;
+        allBalances.push(Balance(from, to, amount));
+    }
+
+    function getAllBalances() external view returns (Balance[] memory) {
+        return allBalances;
+    }
+}
 
 
 }
